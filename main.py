cbc82ae2671f8d472c68d3a1559f5f71578d06c5
Vendetta <vlad.bykov2004@mail.ru>
2022-01-01 14:07:35 +0300

    Add UI

---
+++ main.py
@@ -0,0 +1,20 @@
+import sys
+
+from PyQt6 import uic, QtCore, QtGui, QtWidgets
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    ServerAdress = "http://localhost:5000"
+    MessageID = 0
+
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        uic.loadUi('messanger.ui', self)
+
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    w = MainWindow()
+    w.show()
+    sys.exit(app.exec())
